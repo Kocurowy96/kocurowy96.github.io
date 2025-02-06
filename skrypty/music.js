3892f6ce5262c64b4973e13c404bd7ca1fff86d0
Kocurowy96 <wiktort441@gmail.com>
2025-02-06 19:58:39 +0100

    update

---
+++ skrypty/music.js
@@ -0,0 +1,30 @@
+// Lista utworów do odtwarzania
+const playlist = [
+    "kocurowy96.github.io/muzyka/Ariis - From The Top Phonk NCS - Copyright Free Music.mp3",
+    "utwor2.mp3"
+];
+
+let currentTrack = 0;
+const audioPlayer = document.getElementById('audioPlayer');
+const audioSource = document.getElementById('audioSource');
+
+// Funkcja do zmiany utworu
+function changeTrack(trackIndex) {
+    if (trackIndex >= 0 && trackIndex < playlist.length) {
+        audioSource.src = playlist[trackIndex];
+        audioPlayer.load();
+        audioPlayer.play();
+    }
+}
+
+// Event listener na zakończenie utworu
+audioPlayer.addEventListener('ended', function() {
+    currentTrack++;
+    if (currentTrack >= playlist.length) {
+        currentTrack = 0;
+    }
+    changeTrack(currentTrack);
+});
+
+// Początkowe ustawienie pierwszego utworu
+changeTrack(currentTrack);
